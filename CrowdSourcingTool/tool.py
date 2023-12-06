2d3cd4b8d0b3902682bd8959e7ed6143dbfeb53b
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-12-06 13:23:01 +0200

    add: intial template
    added template index.html and made the root render it

--- CrowdSourcingTool/tool.py
+++ CrowdSourcingTool/tool.py
@@ -6,8 +6,10 @@ i = 1
 @app.route('/')
 def index():
     global i
-    i+=1
-    return str(i)
+    # logic to fetch quesiton here
+    i += 1
+    question = "Replace this text with your question."
+    return render_template('index.html', question=question)
 
 
 if __name__ == '__main__':
