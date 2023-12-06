2462704c642c26c01bb65d1eaba1b67ab383e96f
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-12-06 14:07:57 +0200

    add: logic for fetching answers and showing new questions

--- CrowdSourcingTool/tool.py
+++ CrowdSourcingTool/tool.py
@@ -1,22 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 # mongodb atlas
-mongo_uri = "your_mongodb_atlas_connection_string"
-client = MongoClient(mongo_uri)
-db = client.get_database("your_database_name")
-collection = db.get_collection("your_collection_name")
+# mongo_uri = "your_mongodb_atlas_connection_string"
+# client = MongoClient(mongo_uri)
+# db = client.get_database("your_database_name")
+# collection = db.get_collection("your_collection_name")
 
-i = 0
+i = -1
 
 questions = ["Lorem", "ipsum", "dolor", "sit", "amet"]
 
 @app.route('/')
 def index():
-    global i
-    i = (i + 1) % len(questions)
-    question = questions[i]
+    question = get_next_question()
     return render_template('index.html', question=question)
 
 @app.route('/submit', methods=['POST'])
@@ -27,10 +25,17 @@ def submit():
     # Insert the user's answer into the MongoDB collection
     # collection.insert_one({'answer': user_answer})
     print(user_answer)
-
+    print(i)
+    new_question = get_next_question()
+    
     # Redirect to the index route to fetch a new question
-    return index()
+    return {"new_question": new_question}
 
+
+def get_next_question():
+    global i
+    i = (i + 1) % len(questions)
+    return questions[i]
 
 if __name__ == '__main__':
     app.run(debug=True)
