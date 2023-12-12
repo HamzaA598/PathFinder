dd9cce2aceabda03c471003bd2fcaa17e21623ea
HamzaA598 <hamzaemad598@gmail.com>
2023-12-12 13:31:39 +0200

    add button skip

--- CrowdSourcingTool/tool_sql.py
+++ CrowdSourcingTool/tool_sql.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from questions import QUESTIONS
 
 app = Flask(__name__)
-# mongodb atlas
+# sql db
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="PathFinderTool",
     password="youssif12345",
@@ -17,6 +17,11 @@ db = SQLAlchemy(app)
 
 i = -1
 
+def get_next_question():
+    global i
+    i = (i + 1) % len(QUESTIONS)
+    return QUESTIONS[i]
+
 @app.route('/')
 def index():
     question = get_next_question()
@@ -27,26 +32,24 @@ def submit():
     global i
     # Retrieve user's answer from the form
     user_answer = request.form.get('answer')
+
     answer_record = Answer(text = user_answer, question_id = i)
 
-    # Insert the user's answer into the MongoDB collection
+    # Insert the user's answer into the sql db
 
     db.session.add(answer_record)
     db.session.commit()
 
-    if user_answer is not None:
-        print(user_answer)
-        print(i)
     new_question = get_next_question()
     
     # Redirect to the index route to fetch a new question
     return {"new_question": new_question}
 
 
-def get_next_question():
-    global i
-    i = (i + 1) % len(QUESTIONS)
-    return QUESTIONS[i]
+@app.route('/skip', methods=['GET'])
+def skip():
+    new_question = get_next_question()
+    return {"new_question": new_question}
 
 if __name__ == '__main__':
     app.run(debug=True)
