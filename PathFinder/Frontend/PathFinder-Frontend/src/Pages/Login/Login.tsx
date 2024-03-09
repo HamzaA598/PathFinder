8465cf99fd617453e74345117671ab285fdc2ec1
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-03-09 13:58:13 +0200

    feat: signup and login now are forms and works corectly

--- PathFinder/Frontend/PathFinder-Frontend/src/Pages/Login/Login.tsx
+++ PathFinder/Frontend/PathFinder-Frontend/src/Pages/Login/Login.tsx
@@ -1,12 +1,39 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "@/components/ui/use-toast";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = async () => {
+    try {
+      const response = await axios.post(
+        // login endpoint
+        "",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      // handle response
+      navigate("/");
+    } catch (err) {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your Login.",
+      });
+    }
+  };
+
   return (
     <div key="1" className="flex items-center h-full px-4 mt-20 mb-44">
-      <div className="w-full max-w-lg mx-auto  bg-muted/50 border rounded-lg py-12 p-4 dark:border-slate-700">
+      <div className="w-full max-w-lg mx-auto bg-muted/100 dark:bg-muted/50 border rounded-lg py-12 p-4 dark:border-slate-700">
         <div className="space-y-4 text-center">
           <h1 className="text-3xl font-bold">Login</h1>
           <p className="text-gray-500 dark:text-gray-400">
@@ -14,30 +41,52 @@ export default function Login() {
           </p>
         </div>
 
-        <div className="space-y-4 mt-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              placeholder="m@example.com"
-              required
-              type="email"
-            />
-          </div>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            if (!email?.trim() || !password?.trim()) {
+              return;
+            }
+            await login();
+          }}
+        >
+          <div className="space-y-4 mt-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" required type="password" />
-          </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                required
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
 
-          <Button className="w-full">Login</Button>
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
 
-          <div className="text-center text-sm mt-4">
-            <Link className="text-sm underline " to="#">
-              Forgot your password?
-            </Link>
+            <div className="text-center text-sm mt-4">
+              <Link className="text-sm underline " to="#">
+                Forgot your password?
+              </Link>
+            </div>
           </div>
-        </div>
+        </form>
 
         <div className="text-center text-sm mt-4">
           Don't have an account?{" "}
--- PathFinder/Frontend/PathFinder-Frontend/src/Pages/Signup/Signup.tsx
+++ PathFinder/Frontend/PathFinder-Frontend/src/Pages/Signup/Signup.tsx
@@ -2,12 +2,42 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "@/components/ui/use-toast";
+import axios from "axios";
 
 export default function Signup() {
+  // may need to add more fields or change fields
+  const [email, setEmail] = useState("");
+  const [fName, setFName] = useState("");
+  const [sName, setSName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const signup = async () => {
+    try {
+      const response = await axios.post(
+        //signup endpoint
+        "",
+        {
+          fName: fName,
+          sName: sName,
+          email: email,
+          password: password,
+        }
+      );
+      // handle response
+      navigate("/login");
+    } catch (err) {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your Sign up.",
+      });
+    }
+  };
   return (
     <div key="1" className="flex items-center h-full px-4 mt-20 mb-44">
-      <div className="w-full max-w-lg mx-auto  bg-muted/50 border rounded-lg py-12 p-4 dark:border-slate-700">
+      <div className="w-full max-w-lg mx-auto bg-muted/100 dark:bg-muted/50 border rounded-lg py-12 p-4 dark:border-slate-700">
         <div className="space-y-4">
           <div className="space-y-2 text-center">
             <h1 className="text-4xl font-bold">Sign up</h1>
@@ -15,41 +45,80 @@ export default function Signup() {
               Sign up for an account
             </p>
           </div>
-          <div className="space-y-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="space-y-2">
-                <Label htmlFor="first-name">First Name</Label>
-                <Input id="first-name" placeholder="John" required />
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              if (
+                !email?.trim() ||
+                !password?.trim() ||
+                !fName?.trim() ||
+                !sName?.trim()
+              ) {
+                return;
+              }
+              await signup();
+            }}
+          >
+            <div className="space-y-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="space-y-2">
+                  <Label htmlFor="first-name">First Name</Label>
+                  <Input
+                    id="first-name"
+                    placeholder="John"
+                    required
+                    onChange={(e) => {
+                      setFName(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="last-name">Last Name</Label>
+                  <Input
+                    id="last-name"
+                    placeholder="Doe"
+                    required
+                    onChange={(e) => {
+                      setSName(e.target.value);
+                    }}
+                  />
+                </div>
               </div>
               <div className="space-y-2">
-                <Label htmlFor="last-name">Last Name</Label>
-                <Input id="last-name" placeholder="Doe" required />
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  placeholder="johndoe@example.com"
+                  required
+                  type="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  required
+                  type="password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </div>
+              <Button className="w-full" type="submit">
+                Sign Up
+              </Button>
+              <Separator className="my-8" />
+              <div className="mt-4 text-center text-sm">
+                Already have an account?
+                <Link className="underline" to="/login">
+                  Login
+                </Link>
               </div>
             </div>
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                placeholder="johndoe@example.com"
-                required
-                type="email"
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" required type="password" />
-            </div>
-            <Button className="w-full" type="submit">
-              Sign Up
-            </Button>
-            <Separator className="my-8" />
-            <div className="mt-4 text-center text-sm">
-              Already have an account?
-              <Link className="underline" to="/login">
-                Login
-              </Link>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
