677b6f6bd30ff6da43a75cabf74d7cec3c470122
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-04-27 15:36:30 +0300

    add: basic form validation action

--- PathFinder/Chatbot/actions/actions.py
+++ PathFinder/Chatbot/actions/actions.py
@@ -9,8 +9,9 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 
 class ActionGiveCollegeRecommendation(Action):
@@ -42,6 +43,65 @@ class ActionGiveCollegeRecommendation(Action):
 
         # Return an empty list of events
         return []
+
+
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_recommend_college_form"
+
+    def validate_location(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `location` value."""
+        # TODO: Make sure location is set correctly by validation
+        # you can add print statements to help debugging
+        # example validation
+        # LOGIC TO VALIDATE SLOT_VALUE
+        # if not correct:
+        #     dispatcher.utter_message(text="data entered incorrectly")
+        #     return {"location": None} # reset slot value
+        # return {"location": validated_slot_value}
+        return {"location": slot_value}
+
+    def validate_grade(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `grade` value."""
+        # TODO: Make sure location is set correctly by validation
+        # you can add print statements to help debugging
+        # example validation
+        # LOGIC TO VALIDATE SLOT_VALUE
+        # if not correct:
+        #     dispatcher.utter_message(text="data entered incorrectly")
+        #     return {"grade": None} # reset slot value
+        # return {"grade": validated_slot_value}
+        return {"grade": slot_value}
+
+    def validate_private_college(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `private_college` value."""
+        # TODO: Make sure location is set correctly by validation
+        # you can add print statements to help debugging
+        # example validation
+        # LOGIC TO VALIDATE SLOT_VALUE
+        # if not correct:
+        #     dispatcher.utter_message(text="data entered incorrectly")
+        #     return {"private_college": None} # reset slot value
+        # return {"private_college": validated_slot_value}
+        return {"private_college": slot_value}
 
 
 # class ActionHelloWorld(Action):
