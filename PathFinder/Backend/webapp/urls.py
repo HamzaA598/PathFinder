7c47324ea83e3fc82adcec875ab3c8a9bb9776da
YehiaElHadidi <hadidiyehia@gmail.com>
2024-05-12 14:48:22 +0300

    add edit University and college endpoints

--- PathFinder/Backend/webapp/urls.py
+++ PathFinder/Backend/webapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('University', views.EditUniversity, name='EditUniversity'),
     path('University', views.AllUniversities, name='GetAllUniversities'),
     path('University/<str:id>', views.UniversityInfo, name='GetUniversity'),
     path('University/College/<str:id>', views.CollegeByUniversity, name='GetCollegeByUniversity'),
+
+    path('College', views.EditCollege, name='EditCollege'),
     path('College', views.AllColleges, name='GetAllColleges'),
     path('College/<str:id>', views.CollegeInfo, name='GetCollege'),
 ]
--- PathFinder/Backend/webapp/views.py
+++ PathFinder/Backend/webapp/views.py
@@ -1,9 +1,11 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-
+from django.core.serializers import deserialize
+import json
 from bson import ObjectId
 from django.forms.models import model_to_dict
 from .models import *
@@ -19,14 +21,13 @@ def AllUniversities(request):
 
 @api_view(['GET'])
 def UniversityInfo(request, id):
-    universities = University.objects.all()
-    for uni in universities:
-        if uni._id == ObjectId(id):
-            uni._id = id
-            serializer = UniversitySerializer(uni)
-            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    try:
+        university = University.objects.get(_id=id)
+        return JsonResponse(model_to_dict(university), status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'University not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 # TODO: get all colleges
 # TODO: get all colleges of a university by its id
@@ -40,19 +41,60 @@ def AllColleges(request):
 
 @api_view(['GET'])
 def CollegeInfo(request, id):
-    college = College.objects.all()
-    for col in college:
-        if col._id == ObjectId(id):
-            col._id = id
-            return JsonResponse(model_to_dict(col), status=status.HTTP_200_OK)
-
-    return Response(status=status.HTTP_404_NOT_FOUND)
+    try:
+        college = College.objects.get(_id=id)
+        return JsonResponse(model_to_dict(college), status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'college not found'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
 def CollegeByUniversity(request, id):
-    universities = University.objects.all()
-    for uni in universities:
-        if uni._id == ObjectId(id):
-            colleges = College.objects.filter(universities=uni)
-            colleges = [{'_id': str(college._id), 'name': college.name} for college in colleges]
-            return Response(list(colleges), status=status.HTTP_200_OK)
+    try:
+        colleges = College.objects.filter(university=id)
+        colleges = [{'_id': str(college._id), 'name': college.name} for college in colleges]
+        return Response(list(colleges), status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'University not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+# {
+#     adminId: id
+#     university:{
+#
+#     }
+# }
+@api_view(['PUT'])
+def EditUniversity(request):
+    data = json.loads(request.body.decode('utf-8'))
+    adminId = data['adminId']
+    university = data['university']
+    try:
+        oldUniversity = University.objects.get(_id=str(university['_id']))
+    except University.DoesNotExist:
+        return JsonResponse({'error': 'University not found'}, status=status.HTTP_404_NOT_FOUND)
+    if adminId == oldUniversity.admin_id:
+        serializer = UniversitySerializer(oldUniversity, data=university)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return JsonResponse({'error': 'UNAUTHORIZED'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['PUT'])
+def EditCollege(request):
+    data = json.loads(request.body.decode('utf-8'))
+    adminId = data['adminId']
+    role = data['role']
+    college = data['college']
+    try:
+        oldCollege = University.objects.get(_id=str(college['_id']))
+    except College.DoesNotExist:
+        return JsonResponse({'error': 'University not found'}, status=status.HTTP_404_NOT_FOUND)
+    if adminId == str(oldCollege.admin_id) or (role == "UniversityAdmin" and University.objects.get(_id=oldCollege.university).admin_id) == adminId:
+        serializer = CollegeSerializer(oldCollege, data=college)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return JsonResponse({'error': 'UNAUTHORIZED'}, status=status.HTTP_401_UNAUTHORIZED)
