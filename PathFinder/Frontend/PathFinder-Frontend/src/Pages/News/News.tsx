5b92e33648b86adcab56dd4bea1ae81d2a93ae36
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2024-06-26 16:55:47 +0300

    view announcements in frontend

--- PathFinder/Frontend/PathFinder-Frontend/src/Pages/News/News.tsx
+++ PathFinder/Frontend/PathFinder-Frontend/src/Pages/News/News.tsx
@@ -1,50 +1,47 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface NewsItem {
+  university: string;
+  date: string;
+  description: string;
+}
 
 const News = () => {
-  const News_items = [
-    {
-      university: "Cairo University",
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas laudantium",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas laudantium",
-    },
-    {
-      university: "Cairo University",
-      title: "You have a new message!",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas laudantium",
-    },
-    {
-      university: "Cairo University",
-      title: "Your subscription is expiring soon!",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas laudantium",
-    },
-    {
-      university: "Cairo University",
-      title: "Your subscription is expiring soon!",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas laudantium",
-    },
-  ];
+  const [newsItems, setNewsItems] = useState<NewsItem[]>([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get<NewsItem[]>(
+          "http://localhost:8000/webapp/Announcement"
+        );
+        setNewsItems(response.data);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    };
+
+    fetchNews();
+  }, []);
 
   return (
-    <div className="News_page  w-[1300px]">
-      <Card className=" ml-64 h-screen  items-center justify-center ">
+    <div className="News_page w-[1300px]">
+      <Card className="ml-64 h-screen items-center justify-center">
         <CardHeader>
           <CardTitle>News Page</CardTitle>
         </CardHeader>
         <CardContent className="grid gap-4">
-          {News_items.map((news_item, index) => (
+          {newsItems.map((news_item, index) => (
             <div
               key={index}
               className="mb-6 grid grid-cols-[25px_1fr] items-start pb-6 last:mb-0 last:pb-0 border-b border-gray-200"
             >
               <span className="flex h-3 w-3 translate-y-1 rounded-full bg-emerald-700" />
               <div className="space-y-2 pl-4">
-                <p className="text-lg font-semibold ">{news_item.university}</p>
-                <p className="text-base font-medium">{news_item.title}</p>
+                <p className="text-lg font-semibold">{news_item.university}</p>
+                <p className="text-base font-medium">{news_item.date}</p>
                 <p className="text-sm">{news_item.description}</p>
               </div>
             </div>
