430922d97b62b16992e37cf06a29410c9038ffc4
yahiaashraf92 <yahiahamaid@gmail.com>
2024-07-05 17:03:17 +0300

    added announcements to the uni info page

--- PathFinder/Frontend/PathFinder-Frontend/src/Pages/UniversityInfo/components/Overview.tsx
+++ PathFinder/Frontend/PathFinder-Frontend/src/Pages/UniversityInfo/components/Overview.tsx
@@ -1,25 +1,28 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import axios from "axios";
-import React, { useEffect, useRef } from "react";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { toast } from "@/components/ui/use-toast";
 import { useParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 const Overview = ({ uni_name, user }) => {
-  console.log("user hagattt : " + user.role);
-
   const [isAdmin, setIsAdmin] = useState(false);
-  const [universityInfo, setUniversityInfo] = useState([]);
+  const [universityInfo, setUniversityInfo] = useState({});
   const [isEditing, setIsEditing] = useState(null);
+  const [isAddingNews, setIsAddingNews] = useState(false);
+  const [newAnnouncement, setNewAnnouncement] = useState({
+    college: "",
+    title: "",
+    description: "",
+    date: new Date().toISOString().split("T")[0],
+    university: "", // This will be set once the university data is fetched
+  });
+  const [newsItems, setNewsItems] = useState([]);
 
   const effectRan = useRef(false);
-
   const url = `http://127.0.0.1:8000/webapp/University/name/${uni_name}`;
 
-  console.log("dicnsiucnsievn " + url);
-
-  React.useEffect(() => {
+  useEffect(() => {
     if (effectRan.current) return;
 
     axios
@@ -28,7 +31,12 @@ const Overview = ({ uni_name, user }) => {
         if (response.data.length === 0) {
           throw new Error("EmptyResponse");
         }
-        setUniversityInfo(response.data[0]);
+        const universityData = response.data[0];
+        setUniversityInfo(universityData);
+        setNewAnnouncement((prev) => ({
+          ...prev,
+          university: universityData._id, // Set university ID
+        }));
       })
       .catch((error) => {
         let errorMessage = "Uh oh! Something went wrong.";
@@ -52,11 +60,14 @@ const Overview = ({ uni_name, user }) => {
         });
       });
     effectRan.current = true;
-  }, [universityInfo, url]);
+  }, [url]);
 
   useEffect(() => {
     if (user) {
-      if (user.role == "university_admin" && user.id == universityInfo.admin) {
+      if (
+        user.role === "university_admin" &&
+        user.id === universityInfo.admin
+      ) {
         setIsAdmin(true);
       } else {
         setIsAdmin(false);
@@ -79,7 +90,7 @@ const Overview = ({ uni_name, user }) => {
       );
 
       setUniversityInfo(response.data);
-      setIsEditing(null); // Reset isEditing to null to remove the save button
+      setIsEditing(null);
 
       toast({
         title: "Success",
@@ -108,44 +119,131 @@ const Overview = ({ uni_name, user }) => {
     }
   };
 
+  const handleAddNews = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/webapp/Announcement/add",
+        {
+          announcement: newAnnouncement,
+        },
+        { withCredentials: true }
+      );
+
+      setNewsItems([...newsItems, response.data]);
+      setIsAddingNews(false);
+      toast({
+        title: "Success",
+        description: "Announcement posted successfully.",
+      });
+    } catch (error) {
+      let errorMessage = "Uh oh! Something went wrong.";
+      let errorDesc = "There was a problem with your request.";
+
+      if (error.response) {
+        errorMessage = "Internal Server Error";
+        errorDesc = "Please try again later.";
+      } else if (error.request) {
+        errorMessage = "Network Error";
+        errorDesc =
+          "Couldn't connect to the server. Please check your internet connection.";
+      } else {
+        errorMessage = "Request Error";
+        errorDesc = "An error occurred while setting up the request.";
+      }
+
+      toast({
+        title: errorMessage,
+        description: errorDesc,
+      });
+    }
+  };
+
+  function checkConditions(key: string, value: any): boolean {
+    return key !== "_id" && Boolean(value);
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewAnnouncement((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="grid gap-8">
-      {Object.entries(universityInfo).map(([key, value]) => (
-        <Card key={key}>
-          <CardHeader>
-            <CardTitle>{key}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="content-wrapper">
-              {isEditing === key ? (
-                <input
-                  className="text-black"
-                  type="text"
-                  value={value}
-                  onChange={(e) =>
-                    setUniversityInfo({
-                      ...universityInfo,
-                      [key]: e.target.value,
-                    })
-                  }
-                />
-              ) : (
-                <div className="content-text">{value}</div>
-              )}
-              {isAdmin && (
-                <Button
-                  className="edit-button"
-                  onClick={() =>
-                    isEditing === key ? handleSave() : handleEdit(key)
-                  }
-                >
-                  {isEditing === key ? "save" : "edit"}
-                </Button>
-              )}
+      {isAdmin && (
+        <div className="announcement-section">
+          <Button onClick={() => setIsAddingNews(!isAddingNews)}>
+            {isAddingNews ? "Cancel" : "Add Announcement"}
+          </Button>
+          {isAddingNews && (
+            <div className="announcement-form">
+              <input
+                type="text"
+                name="college"
+                placeholder="College"
+                value={newAnnouncement.college}
+                onChange={handleInputChange}
+                className="text-black"
+              />
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                value={newAnnouncement.title}
+                onChange={handleInputChange}
+                className="text-black"
+              />
+              <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                value={newAnnouncement.description}
+                onChange={handleInputChange}
+                className="text-black"
+              />
+              <Button onClick={handleAddNews}>Post Announcement</Button>
             </div>
-          </CardContent>
-        </Card>
-      ))}
+          )}
+        </div>
+      )}
+      {Object.entries(universityInfo).map(
+        ([key, value]) =>
+          checkConditions(key, value) && (
+            <Card key={key}>
+              <CardHeader>
+                <CardTitle>{key}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="content-wrapper">
+                  {isEditing === key ? (
+                    <input
+                      className="text-black"
+                      type="text"
+                      value={value}
+                      onChange={(e) =>
+                        setUniversityInfo({
+                          ...universityInfo,
+                          [key]: e.target.value,
+                        })
+                      }
+                    />
+                  ) : (
+                    <div className="content-text">{value}</div>
+                  )}
+                  {isAdmin && (
+                    <Button
+                      className="edit-button"
+                      onClick={() =>
+                        isEditing === key ? handleSave() : handleEdit(key)
+                      }
+                    >
+                      {isEditing === key ? "Save" : "Edit"}
+                    </Button>
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+          )
+      )}
     </div>
   );
 };
